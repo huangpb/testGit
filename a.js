85f390051734a9b3ad6e347a2fdec73ba011bd1a
hpb <yimi.hpb@raycloud.com>
2019-12-22 22:51:00 +0800

    add

--- a.js
+++ a.js
@@ -1,11 +1,4 @@
 var a = 1;
-
-const foo = function() {}
-
-const number = 12;
-const bb;
-const reset1 = '1';
-const time1 = 1;
-const time5 = 1;
-var b = a * 2;
-const text = '1111';
+'11111'
+'2222'
+'3333'
