a28d7583d7b72677688f6e0b5919d20a4ee921c7
hpb <yimi.hpb@raycloud.com>
2020-06-06 10:13:17 +0800

    8888

--- index.js
+++ index.js
@@ -2,11 +2,4 @@ var a = 1;
 var b = 2;
 var arr = [1,2,3,4];
 let text = 'master文字1'
-const master;
-const ab;
-const reset1;
-const reset2;
-let str2 = 'ab 上的文字';
-str2 = 'ab 上的文字3';
-222
-3333
+'8888'
